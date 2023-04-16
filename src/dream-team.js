dc91ba2fb0035645da216298142795de9d00c1a8
Zavadskaya Olga <zavadskaya.ov@gmail.com>
2023-04-16 12:53:42 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  let dreamTeam = [];
+  members.forEach((e) => {
+    if (typeof e === 'string') {
+      dreamTeam.push(e.split(/\s/).join('').slice(0, 1).toUpperCase());
+    }
+  });
+ 
+  return  dreamTeam.sort().join('');
 }
 
 module.exports = {
